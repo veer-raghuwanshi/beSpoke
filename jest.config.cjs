64ccf34514128dbd132b8457ad02fc96f620a4d6
Veer <candidate@local>
2026-07-11 22:30:20 +0530

    test: configure ESM integration runner

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,1 +1,1 @@
-module.exports = { preset: 'ts-jest', testEnvironment: 'node', testMatch: ['**/*.test.ts'] };
+module.exports = { preset: 'ts-jest', testEnvironment: 'node', testMatch: ['**/*.test.ts'], moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' } };
